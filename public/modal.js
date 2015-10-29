49cdddc90cbf0854a3d5ab23c55818be7b532ccd
Adamantish <sonsequence@hotmail.com>
2015-10-29 15:42:14 +0000

    Modal open and close works with correct title.

---
+++ public/modal.js
@@ -0,0 +1,30 @@
+
+var MyModal = function(title, content){
+  this.title = title
+  this.content = content
+}
+
+MyModal.prototype.render = function() {
+
+  var container = document.createElement("div");
+  container.setAttribute("class", "modal-background");
+
+  var modal = document.createElement("div");
+  modal.setAttribute("class", "my-modal");
+  modal.setAttribute("onclick","app.removeMyModal()")
+
+  var heading = document.createElement('h3');
+  heading.innerText = this.title
+  var paragraph = document.createElement('p');
+  paragraph.innerText = this.description
+
+  modal.appendChild(heading)
+  modal.appendChild(paragraph)
+  container.appendChild(modal)
+
+  return container;
+
+}
+
+// MyModal.prototype.
+
--- public/my_soundcloud.js
+++ public/my_soundcloud.js
@@ -1,6 +1,26 @@
-SC.initialize({
-  client_id: 'fd69671c33c9f89d3015a61724293122'
-});
+
+function App() {
+
+  // TODO: move this into API
+    SC.initialize({
+    client_id: 'fd69671c33c9f89d3015a61724293122'
+  });
+
+  this.catalog = new Catalog()
+    // console.log("SC re-initialized")
+
+};
+
+App.prototype.showMyModal = function() {
+  this.modal = new MyModal(this.title, this.content)
+  var element = this.modal.render()
+  document.getElementsByTagName('body')[0].appendChild(element)
+}
+
+App.prototype.removeMyModal = function() {
+  var remove_me = document.getElementsByClassName('modal-background')[0]
+  remove_me.parentNode.removeChild(remove_me)
+}
 
 function API() {};
 
@@ -12,26 +32,30 @@ API.prototype.doSearch = function() {
 
   // Search soundcloud for artists
   SC.get('/tracks', { q: searchTerm, license: 'cc-by-sa' }, function(apiTracks) {
-      var catalog = new Catalog
-      catalog.innerHTML = ""
+      
+      app.catalog.innerHTML = ""
       for(i in apiTracks) {
 
         var t = apiTracks[i]
 
-        track = new Track(catalog, t.title, t.artwork_url, t.description)
-
+        track = new Track(app.catalog, t.title, t.artwork_url, t.description)
         track.addMeToCatalog()
 
       }
     }
   );
 };
+
 function Catalog() {
 
-  this.element = document.getElementById("catalog")
+  this.tracks = [];
+  this.element = document.getElementById("catalog");
 
-  return this.element
+  // return this.element
+
 };
+
+Catalog.prototype = new App()
 
 function Track(catalog, title, artwork_url, description, numOfPlays) {
 
@@ -41,7 +65,11 @@ function Track(catalog, title, artwork_url, description, numOfPlays) {
   this.description = description;
   this.numOfPlays = numOfPlays
 
+  this.makeModalContent()
+
 };
+
+Track.prototype = new Catalog()
 
 Track.prototype.addMeToCatalog = function() {
 
@@ -49,7 +77,10 @@ Track.prototype.addMeToCatalog = function() {
         catalogEntry.setAttribute("class","catalog-entry")
 
         var image = document.createElement("img")
+        var catalogIndex = app.catalog.tracks.length
+
         image.setAttribute("src", this.artwork_url || "")
+        image.setAttribute("onclick", "app.catalog.tracks[" + catalogIndex + "].showMyModal()")
 
         var entryTitle = document.createElement("span")
         entryTitle.innerText = this.title
@@ -57,11 +88,19 @@ Track.prototype.addMeToCatalog = function() {
         catalogEntry.appendChild(image)
         catalogEntry.appendChild(entryTitle)
 
-        catalog.appendChild(catalogEntry)
+        app.catalog.element.appendChild(catalogEntry)
+        app.catalog.tracks.push(this)
 
 }
 
-var api = new API
+Track.prototype.makeModalContent = function() {
+
+  this.content = this.description;
+  // return this.content
+}
+
+var api = new API()
+var app = new App()
 
 
 // function createCatalogEntry ()
